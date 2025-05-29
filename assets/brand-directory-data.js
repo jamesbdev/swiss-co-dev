14c6fdae9a2219936bdd76eb18b711cb19ab6371
James Bremner <j.bremner@ymail.com>
2025-05-29 13:33:17 +0100

    upload theme files

---
+++ assets/brand-directory-data.js
@@ -0,0 +1,29 @@
+var brandDirectoryData = {
+  "brands": [
+    { "name": "Audemars Piguet", "url": "/collections/audemars-piguet" },
+    { "name": "Bell & Ross", "url": "/collections/bell-ross" },
+    { "name": "Breitling", "url": "/collections/breitling" },
+    { "name": "Bulgari", "url": "/collections/bvlgari" },
+    { "name": "Cartier", "url": "/collections/cartier" },
+    { "name": "Chopard", "url": "/collections/chopard" },
+    { "name": "Grand Seiko", "url": "/collections/grand-seiko" },
+    { "name": "Hamilton", "url": "/collections/hamilton" },
+    { "name": "Hermes", "url": "/collections/hermes" },
+    { "name": "Hublot", "url": "/collections/hublot" },
+    { "name": "IWC", "url": "/collections/iwc" },
+    { "name": "Jaeger-LeCoultre", "url": "/collections/jaeger-lecoultre" },
+    { "name": "Longines", "url": "/collections/longines" },
+    { "name": "Maurice Lacroix", "url": "/collections/maurice-lacroix" },
+    { "name": "Montblanc", "url": "/collections/montblanc" },
+    { "name": "Nomos Glashütte", "url": "/collections/nomos" },
+    { "name": "Omega", "url": "/collections/omega" },
+    { "name": "Oris", "url": "/collections/oris" },
+    { "name": "Panerai", "url": "/collections/panerai" },
+    { "name": "Rolex", "url": "/collections/rolex" },
+    { "name": "Seiko", "url": "/collections/seiko" },
+    { "name": "TAG Heuer", "url": "/collections/tag-heuer" },
+    { "name": "Tissot", "url": "/collections/tissot" },
+    { "name": "Tudor", "url": "/collections/tudor" },
+    { "name": "Zenith", "url": "/collections/zenith" }
+  ]
+};
